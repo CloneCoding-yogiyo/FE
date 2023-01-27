4d1b8f84c108d5df7a5ae35edb8a0384750f74bc
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-01-27 20:07:08 +0900

    스토어, 메뉴, 장바구니,  주문완료페이지 기능위주구현.... 하 인생.. 못해먹겠네.

--- yogiyo/src/components/Store.jsx
+++ yogiyo/src/components/Store.jsx
@@ -14,7 +14,7 @@ export default function Store() {
   const fetchTodos = async () => {
     const { data } = await axios.get('http://localhost:3001/Store');
     // const { data } = await axios.get('http://3.36.130.126/stores');
-    // const { data } = await axios.get('http://13.209.12.254/store/menus');
+
     console.log(data);
     setStores(data);
   };
