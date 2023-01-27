8d6a8bb936b51dcfc2259cf609a08c1e29583e21
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-01-27 12:01:22 +0900

    오더컴플리트 작업하기전 브렌치 만들기 위함..

--- yogiyo/src/pages/OrderComplete.jsx
+++ yogiyo/src/pages/OrderComplete.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function OrderComplete() {
+  const globaladdCart = useSelector((state) => state.menuList.menuList);
+
   return (
     <div>
       <div>주문완료</div>
