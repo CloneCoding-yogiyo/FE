e1d761335a87baff35ef87ba91465fa911238495
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-01-27 12:16:12 +0900

    주문완료창 기능 ㅇㅋ

--- yogiyo/src/pages/OrderComplete.jsx
+++ yogiyo/src/pages/OrderComplete.jsx
@@ -7,6 +7,36 @@ export default function OrderComplete() {
   return (
     <div>
       <div>주문완료</div>
+      <div>
+        {globaladdCart?.map((cartMenu) => {
+          console.log(globaladdCart);
+          return (
+            <>
+              <div>
+                {/* <img
+                        src={cartMenu.imageUrl}
+                        class='card-img-top'
+                        alt='...'
+                        width='10px'
+                        height='500px'
+                      ></img> */}
+
+                <div>
+                  <div> {cartMenu.menuName}</div>
+                </div>
+
+                <div>
+                  <div>{cartMenu.amount}</div>
+                </div>
+
+                <div>
+                  <div>{cartMenu.price}원</div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
